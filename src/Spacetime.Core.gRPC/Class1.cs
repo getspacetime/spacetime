e964b361f9f3ec1ba8f4a33ea9c43cb2621eea8e
Cody Mullins <joshua.c.mullins@gmail.com>
2022-03-28 18:50:08 -0400

    add grpc project library and nuget package spacetime.grpcurl

---
+++ src/Spacetime.Core.gRPC/Class1.cs
@@ -0,0 +1,14 @@
+﻿using Spacetime.gRPC.Wrapper;
+
+namespace Spacetime.Core.gRPC
+{
+    public class Class1
+    {
+        public void Test(string importPath, string protoFileName)
+        {
+            var curl = new GRPCurl();
+            var methods = curl.ListMethods(importPath, protoFileName);
+
+        }
+    }
+}
