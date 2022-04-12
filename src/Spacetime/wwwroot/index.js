07cf6f9d307fe6b176db013c028b38b46a4e0771
Cody Mullins <joshua.c.mullins@gmail.com>
2022-04-12 19:43:24 -0400

    fix #1

--- src/Spacetime/wwwroot/index.js
+++ src/Spacetime/wwwroot/index.js
@@ -1,30 +1,32 @@
 import './main.css';
 import * as monaco from 'monaco-editor';
 
-window.something = () => {
-  console.log('well hello');
+window._editors = []
+
+function findEditor(name) {
+    return window._editors.find(editor => editor.name === name);
 }
 
-window._editor = undefined;
+window.editor = (element, name, data) => {
+    console.log('initialize editor', element);
+    const editor = monaco.editor.create(element, {
+        value: data,
+        language: 'json',
+        automaticLayout: true,
+        theme: 'vs-dark'
+    });
 
-window.editor = (element, data) => {
-  console.log('initialize editor', element);;
-  window._editor = monaco.editor.create(element, {
-    value: data,
-    language: 'json',
-    automaticLayout: true,
-    theme: 'vs-dark'
-  });
+    window._editors.push({ name: name, editor });
 }
 
-window.updateEditor = (element, data) => {
-  const editor = window._editor;
-  console.log('editor', editor);
-  if (editor != null) {
-    const model = editor.getModel();
-    console.log('model', model);
-    if (model != null) {
-      model.setValue(data);
+window.updateEditor = (element, name, data) => {
+    const editor = findEditor(name)
+    console.log('editor', editor);
+    if (editor != null) {
+        const model = editor.editor.getModel();
+        console.log('model', model);
+        if (model != null) {
+            model.setValue(data);
+        }
     }
-  }
 }
